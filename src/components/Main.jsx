5fbbad8ca6e0a1fd44ed3f8ecc8e6ba17451353a
Zee <viermarzo@gmail.com>
2024-01-24 00:05:06 +0700

    grid

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import axios from 'axios';
 
 const Main = () => {
-    const [randomBooks, setRandomBooks] = React.useState([]);
+    const [books, setBooks] = React.useState([]);
     React.useEffect(() => {
-        const fetchRandomBooks = async () => {
-            try {
-                const response = await axios.get('https://openlibrary.org/search.json?q=harry+potter');
-                setRandomBooks(response.data.entries);
-            } catch (error) {
-                console.error(error);
-            }
+        const fetchBooksBySearch = async () => {
+            const response = await axios.get('https://openlibrary.org/search.json?title=harry+potter');
+            setBooks(response.data.docs);
         };
-        fetchRandomBooks();
+        fetchBooksBySearch();
     }, []);
     return (
-        <div></div>
+        <div className="mt-16 grid grid-cols-7">
+            {books.map(book => (
+                <div className="inline-block w-[230px] h-[390px] m-[20px] p-[10px] shadow-[0_0_20px_#000]">
+                    {book.isbn && book.isbn[0] && (
+                        <img src={`http://covers.openlibrary.org/b/isbn/${book.isbn[0]}-L.jpg`} alt={book.title} className="border-solid border-2 border-[#808080]" />
+                    )}
+                </div>
+            ))}
+        </div>
     );
 }
 export default Main;
