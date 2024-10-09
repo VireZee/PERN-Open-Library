7dc111e6f52cc5688131e68f8ba786aa961fc197
Zee <56460839+VireZee@users.noreply.github.com>
2024-10-09 22:23:13 +0700

    temp

--- server/src/controllers/AddBookToCollection.ts
+++ server/src/controllers/AddBookToCollection.ts
@@ -6,6 +6,28 @@ const AddBookToCollection = async (req: Request, res: Response) => {
     try {
         const bookRepo = AppDataSource.getRepository(Book)
         const { user_id, cover, title, author, isbn } = req.body
+        const bookCollection = await bookRepo.findOne({
+            where: {
+                user_id,
+                cover_i: cover,
+                title,
+                author,
+                isbn
+            }
+        })
+        if (bookCollection) {
+
+        } else {
+            const newCollection = bookRepo.create({
+                user_id,
+                cover_i: cover,
+                title,
+                author,
+                isbn,
+                created: new Date()
+            })
+            await bookRepo.save(newCollection)
+        }
         return res.status(200).json()
     } catch {
         return res.status(500).json()
