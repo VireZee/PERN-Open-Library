a699fc0dc5a7dcc18e2663d60b89cf4e255857c8
Vire <56460839+VireZee@users.noreply.github.com>
2024-12-30 09:28:03 +0700

    update dependencies

--- server/src/controllers/SignOut.ts
+++ server/src/controllers/SignOut.ts
@@ -1,12 +1,7 @@
 import { Request, Response } from 'express'
 
 const SignOut = (_: Request, res: Response) => {
-    res.clearCookie('!', {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: "strict",
-        priority: "high"
-    })
+    res.clearCookie('!')
     res.status(200).json()
 }
 export default SignOut
