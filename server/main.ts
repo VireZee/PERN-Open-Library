154f9fd654c37114a5c9918ada360e22d84ac3f6
Vire <56460839+VireZee@users.noreply.github.com>
2025-01-23 14:47:00 +0700

    temporary combine API

--- server/main.ts
+++ server/main.ts
@@ -1,48 +1,48 @@
-import AppDataSource from './src/DataSource'
-import express from 'express'
-import http from 'http'
-import cors from 'cors'
-import cp from 'cookie-parser'
-import { ApolloServer } from '@apollo/server'
-import { expressMiddleware } from '@apollo/server/express4'
-import { typeDefs, resolvers } from './src/graphql/GraphQL'
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
-
-AppDataSource.initialize()
-const app = express()
-const httpServer = http.createServer(app)
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
-});
-(async () => {
-    await server.start()
-    app.use(
-        '/graphql',
-        cors<cors.CorsRequest>({ origin: `http://${process.env.DOMAIN}:${process.env.CLIENT_PORT}`, credentials: true }),
-        express.json(),
-        cp(),
-        expressMiddleware(server, {
-            context: async ({ req }) => ({ req })
-        })
-    )
-    httpServer.listen(process.env.PORT)
-})()
 // import AppDataSource from './src/DataSource'
 // import express from 'express'
+// import http from 'http'
 // import cors from 'cors'
 // import cp from 'cookie-parser'
-// import AuthRt from './src/routes/Route'
+// import { ApolloServer } from '@apollo/server'
+// import { expressMiddleware } from '@apollo/server/express4'
+// import { typeDefs, resolvers } from './src/graphql/GraphQL'
+// import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
 
 // AppDataSource.initialize()
 // const app = express()
-// app.use(cors({
-//     origin: 'http://localhost:3000',
-//     credentials: true
-// }))
-// app.use(express.json())
-// app.use(cp())
-// app.listen(process.env.PORT)
+// const httpServer = http.createServer(app)
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+// });
+// (async () => {
+//     await server.start()
+//     app.use(
+//         '/graphql',
+//         cors<cors.CorsRequest>({ origin: `http://${process.env.DOMAIN}:${process.env.CLIENT_PORT}`, credentials: true }),
+//         express.json(),
+//         cp(),
+//         expressMiddleware(server, {
+//             context: async ({ req }) => ({ req })
+//         })
+//     )
+//     httpServer.listen(process.env.PORT)
+// })()
+import AppDataSource from './src/DataSource'
+import express from 'express'
+import cors from 'cors'
+import cp from 'cookie-parser'
+import AuthRt from './src/routes/Route'
 
-// app.use(AuthRt)
+AppDataSource.initialize()
+const app = express()
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+app.use(express.json())
+app.use(cp())
+app.listen(process.env.PORT)
+
+app.use(AuthRt)
