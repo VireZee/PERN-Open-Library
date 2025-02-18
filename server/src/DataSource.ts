f73b6c5437d029b0760b947ae29fc62c9944d46d
Vire <56460839+VireZee@users.noreply.github.com>
2025-02-18 14:18:32 +0700

    !

--- server/src/DataSource.ts
+++ server/src/DataSource.ts
@@ -3,11 +3,11 @@ import './configs/env'
 
 const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST!,
+    host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER!,
-    password: process.env.DB_PASS!,
-    database: process.env.DB_NAME!,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     entities: ["src/models/*.ts"], // make similar like import dotenv from 'dotenv'    import path from 'path'  dotenv.config({ path: path.join(__dirname, '.env') })
     synchronize: process.env.NODE_ENV === 'development'
 })
