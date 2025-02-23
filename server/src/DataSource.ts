631567930c8a26a0c8586eaca4b06991f8d9675e
Zee <56460839+VireZee@users.noreply.github.com>
2025-02-23 18:59:26 +0700

    .

--- server/src/DataSource.ts
+++ server/src/DataSource.ts
@@ -2,6 +2,12 @@ import { DataSource } from 'typeorm'
 import './configs/env'
 import path from 'path'
 
+const getEntitiesPath = () => process.env.NODE_ENV === 'production'
+    ? path.join(__dirname, 'models', '*.js')
+    : path.join(__dirname, 'models', '*.ts')
+const getMigrationPath = () => process.env.NODE_ENV === 'production'
+    ? path.join(__dirname, 'migrations', '*.js')
+    : path.join(__dirname, 'migrations', '*.ts')
 const AppDataSource = new DataSource({
     type: "postgres",
     host: process.env.DB_HOST,
@@ -9,7 +15,8 @@ const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    entities: [path.join(__dirname, 'models', '*')],
-    synchronize: true
+    entities: [getEntitiesPath()],
+    migrations: [getMigrationPath()],
+    synchronize: process.env.NODE_ENV === 'development'
 })
 export default AppDataSource
